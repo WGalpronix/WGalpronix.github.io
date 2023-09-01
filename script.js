6bbaf7060858bb6b612dd6d1d5327df77b3698dd
WGalpronix <143819474+WGalpronix@users.noreply.github.com>
2023-09-02 00:58:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Sample JavaScript code
+// Log a message when the page is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
   console.log('Alpronix website is ready!');
 });
